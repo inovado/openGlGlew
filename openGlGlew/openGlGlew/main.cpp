d310c7e11ec4e71d65ebc8997021cd819852f835
Drack <nautilus_com@yahoo.com>
2021-01-18 15:23:35 -0600

    configuracion inicial de bibliotecas y primer ventana

---
+++ openGlGlew/openGlGlew/main.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const GLint width = 800, height = 600;
+
+int main()
+{
+	//inicializacion GLFW
+	if (!glfwInit())
+	{
+		printf("GLFW initialization failed");
+		glfwTerminate();
+		return 1;
+	}
+
+	// setup GLFW window properties
+	// openGL version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// Core profile = No backwards Compatibility
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// Allow forward compatibility
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* mainWindow = glfwCreateWindow(width, height, "Test Window", NULL, NULL);
+	if (!mainWindow)
+	{
+		printf("GLFW window creation failed");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Get buffer size information
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(mainWindow, &bufferHeight, &bufferWidth);
+
+	// Set context for GLEW to use
+	glfwMakeContextCurrent(mainWindow);
+
+	// Allow modern extension features
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("GLEW  initialization failed!");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup Viewport size
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	// Loop until window closed
+	while (!glfwWindowShouldClose(mainWindow))
+	{
+		// Get + Handle user input events
+		glfwPollEvents();
+
+		// Clear window
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(mainWindow);
+	}
+
+
+
+	return 0;
+}
