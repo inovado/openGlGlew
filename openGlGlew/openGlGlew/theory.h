f3fdc61622c99457f43aca6d595f2f9ec68fd82e
Drack <nautilus_com@yahoo.com>
2021-02-02 18:22:55 -0600

    Shaders and the rendering pipeline

---
+++ openGlGlew/openGlGlew/theory.h
@@ -0,0 +1,60 @@
+/* Teoria
+
+GLFW (creación de contexto) + GLEW (carga de extensión) y tendrá una configuración 
+completamente completa para aplicaciones OpenGL multiplataforma que funcionan en 
+Windows, Mac y Linux y son compatibles con todas las extensiones OpenGL modernas.
+
+Básicamente, GLFW creará una ventana OpenGL para que pueda usar; sin embargo, 
+no es responsable de cargar extensiones de OpenGL (funciones que le permiten 
+hacer cosas modernas de OpenGL como usar sombreadores o FBO / VBO, etc. Por lo 
+tanto, deberá llamar a la función de inicio de GLEW para obtener acceso a ellas. 
+Implementar GLEW en su código no debería ser más que incluir el encabezado y 
+llamar a GlewInit ();
+
+
+The rendering pipiline Stages
+
+1. Vertex Specification
+2. Vertex Shader(programmable)
+3. Tessellation (programmable)
+4. Geometry shader (programmable)
+5. Vertex Post-Processing
+6. Primitive Assembly
+7. Rasterization
+8. Fragment shader(programmable)
+9. Per-sample operations
+
+204----------------------Shaders and the rendering pipeline----------------------------
+
+- Tessellation
+- Geometry Shader
+
+- Rasterization
+	- Converts primitives in to "Fragments".
+	- Fragments are pieces of data for each pixel, obtained from the rasterization process
+	- Fragments data will be interpolated based on its position relative to each vertex.
+
+* Creating a shader program
+
+1.Create empty program
+2.Create empty shaders
+3.Attach shader source code to shaders
+4.Compile shaders
+5.Attach shders to program
+6.Link program (creates executables from shaders and links them together)
+4.Validate program (optional but highly advised because debbugging shaders is a pain).
+
+Summary
+- Rendering pipeline consist of several stages
+- Four stages are programmable via shaders ( Vertex, Tessellation, Geometry, Fragment)
+- Vertex shader is mandatory
+- Vertices: user-defined points in space
+- Primitives: Groups of vertices that make a simple shape ( usually a triangle).
+- Fragments: Per-pixel data created from primitives
+- Vertex Array Object(VAO): WHAT data a vertex has 
+- Vertex Buffer Object(VBO): The vertex data itself
+- Shader programs are created wieht at least a vertex shader and then activated before use.
+
+
+
+*/
