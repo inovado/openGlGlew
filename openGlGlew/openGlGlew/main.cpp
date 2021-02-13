d8ac26462c5241c724b255051f67145ad2f4c78a
Drack <nautilus_com@yahoo.com>
2021-02-13 15:14:45 -0600

    Indexed Draws

--- openGlGlew/openGlGlew/main.cpp
+++ openGlGlew/openGlGlew/main.cpp
@@ -17,7 +17,7 @@
 const GLint width = 800, height = 600;
 const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO, shader, uniformModel; // VAO-matriz de vertices  VBO-bufer de vertices
+GLuint VAO, VBO, IBO, shader, uniformModel; // VAO-matriz de vertices  VBO-bufer de vertices
 
 bool direction = true;
 float triOffset = 0.001f;
@@ -45,7 +45,7 @@ uniform mat4 model;														\n\
 																		\n\
 void main()																\n\
 {																		\n\
-//gl_Position = model * vec4(0.01 * pos.x, 0.01* pos.y, pos.z, 1.0);	\n\
+//gl_Position = model * vec4(0.1 * pos.x, 0.1* pos.y, pos.z, 1.0);	\n\
 gl_Position = vec4(0.4 * pos.x, 0.4* pos.y, pos.z, 1.0);				\n\
 	//gl_Position = model * vec4(pos, 1.0);								\n\
 //gl_Position = vec4(pos, 1.0);											\n\
@@ -71,14 +71,26 @@ void main()														\n\
 //crear trianulo
 void CreateTriangle()
 {
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0 ,1, 2 
+	};
+
 	GLfloat vertices[] = {
 		-1.0f, -1.0f, 0.0f,
+		 0.0f,-1.0f,1.0f,
 		 1.0f, -1.0f, 0.0f,
 		 0.0f,  1.0f, 0.0f
 	};
 
 	glGenVertexArrays(1, &VAO); //cantidad de matrices que se desea crear
 	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -88,6 +100,9 @@ void CreateTriangle()
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+
 	glBindVertexArray(0);
 }
 
@@ -202,6 +217,8 @@ int main()
 		return 1;
 	}
 
+	glEnable(GL_DEPTH_TEST);
+
 	// Setup Viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
@@ -250,22 +267,26 @@ int main()
 
 		// Clear window
 		glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glUseProgram(shader);
 
 		glm::mat4 model;
 		
 		//model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f)); // primero debe ir la escala
-		//model = glm::translate(model, glm::vec3(0.0001f, 0.0001f , 0.0001f)); // sentido en el que se movera el shader, ejes x, y, z
+		model = glm::translate(model, glm::vec3(0.0001f, 0.0001f , 0.0001f)); // sentido en el que se movera el shader, ejes x, y, z
 		//model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.01f, 0.01f, 0.01f));
 		
 
-		glUniform1f(uniformModel, triOffset); 
+		//glUniform1f(uniformModel, triOffset); 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 		glBindVertexArray(0);
 
 		glUseProgram(0);
